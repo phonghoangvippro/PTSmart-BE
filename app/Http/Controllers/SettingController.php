c34498834e4da8e8fb72af4dd5a16d1c8aa594cb
Hoàng Phong <hoangphongmobile1704@gmail.com>
2026-03-20 15:20:51 +0700

    api footer

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -18,14 +18,23 @@ class SettingController extends Controller
     {
         $request->validate([
             'site_name' => 'nullable|string|max:255',
+            'site_description' => 'nullable|string|max:500',
             'support_email' => 'nullable|email|max:255',
             'hotline' => 'nullable|string|max:50',
             'working_hours' => 'nullable|string|max:255',
             'default_shipping_fee' => 'nullable|numeric|min:0',
-            'site_logo' => 'nullable|image|max:2048', // 2MB
+            'site_address' => 'nullable|string|max:500',
+            'social_facebook' => 'nullable|url|max:500',
+            'social_instagram' => 'nullable|url|max:500',
+            'social_youtube' => 'nullable|url|max:500',
+            'site_logo' => 'nullable|image|max:2048',
         ]);
 
-        $keys = ['site_name', 'support_email', 'hotline', 'working_hours', 'default_shipping_fee'];
+        $keys = [
+            'site_name', 'site_description', 'support_email', 'hotline',
+            'working_hours', 'default_shipping_fee', 'site_address',
+            'social_facebook', 'social_instagram', 'social_youtube',
+        ];
 
         foreach ($keys as $key) {
             if ($request->has($key)) {
--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -14,10 +14,15 @@ class SettingSeeder extends Seeder
     {
         $settings = [
             ['key' => 'site_name', 'value' => 'PTSmart'],
+            ['key' => 'site_description', 'value' => 'Chuyên cung cấp các thiết bị điện tử chính hãng với mức giá cạnh tranh nhất thị trường. Hệ thống 64 cửa hàng toàn quốc.'],
             ['key' => 'support_email', 'value' => 'support@ptsmart.vn'],
-            ['key' => 'hotline', 'value' => '1900 6789'],
+            ['key' => 'hotline', 'value' => '1900 8888'],
             ['key' => 'working_hours', 'value' => '08:00 - 21:00 (Hàng ngày)'],
             ['key' => 'default_shipping_fee', 'value' => '30000'],
+            ['key' => 'site_address', 'value' => '123 Nguyễn Huệ, Quận 1, TP.HCM'],
+            ['key' => 'social_facebook', 'value' => 'https://facebook.com/ptsmart'],
+            ['key' => 'social_instagram', 'value' => 'https://instagram.com/ptsmart'],
+            ['key' => 'social_youtube', 'value' => 'https://youtube.com/@ptsmart'],
         ];
 
         foreach ($settings as $setting) {
